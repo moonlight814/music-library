7b8ae302d6be9ff2eea4ff27f8841be97a497664
Natalia <natyargumedo@hotmail.com>
2022-04-20 22:09:46 -0700

    content not showing yet

--- src/App.js
+++ src/App.js
@@ -8,14 +8,38 @@ function App() {
   let[message, setMessage]= useState("Search for Music!")
   let[data, setData]= useState([])
   
-  return (
-    <div className="App">
-    <SearchBar/> 
-    <br/>
-      {message}
-      <Gallery/>
+  const API_URL= "https://itunes.apple.com/search?term="
+  
+  useEffect(() => {
+    if (search) {
+		const fetchData = async () => {
+			document.title = `${search} Music`
+			const response = await fetch(API_URL+search)
+			const resData = await response.json()
+			if (resData.results.length > 0) {
+				setData(resData.results)
+			} else {
+				setMessage('Not Found')
+			}
+		}
+		fetchData()
+  }
+	}, [search])
+
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+}
+
+return (
+    <div>
+        <SearchBar handleSearch = {handleSearch} />
+        {message}
+        <Gallery data={data}/>
     </div>
-  );
+  )
+
 }
 
 export default App;
