f249b522a0d95dd1834b0dc1b54308358cca0714
Natalia <natyargumedo@hotmail.com>
2022-04-23 10:48:08 -0700

    refactored to use with context

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,15 @@
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
-function Gallery(props){
+function Gallery(){
+    const data = useContext(DataContext)
     
-    const display= props.data.map((item,index) =>{
-    return(
-        <GalleryItem item={item} key={index}/>
-    )
+    const display = data.map((item,index) => {
+        return (
+            <GalleryItem item={item} key={index} />
+        )
     })
-    
     return (
         <div>
             {display}
