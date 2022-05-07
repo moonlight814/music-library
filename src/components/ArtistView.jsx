635d2450068f348a17b3f75a0f805392890ec9fa
Natalia <natyargumedo@hotmail.com>
2022-05-07 07:36:27 -0700

    proxy server installed and renders the right components onto page

---
+++ src/components/ArtistView.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import { useParams , Link} from 'react-router-dom'
+
+
+function ArtistView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+    
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i} >
+                <Link to ={`/album/${album.collectionId}`}>
+                <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+    
+    return (
+        <div>
+            {renderAlbums}
+        </div>
+    )
+    
+}
+
+
+export default ArtistView
